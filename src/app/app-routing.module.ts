a09b352244323c922ab761fd1941e210359514dd
Antony Derham <antonyderham@gmail.com>
2017-03-10 15:44:09 +0000

    Cleaned up rules and points pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule } from '@angular/router';
 
 import { HomeComponent } from "./home/home.component";
 import { DeckTestComponent } from "./deck-test/deck-test.component";
+import { PointsComponent } from "./points/points.component";
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
-      { path: 'deck-test', component: DeckTestComponent }
+      { path: 'deck-test', component: DeckTestComponent },
+      { path: 'points', component: PointsComponent }
     ])
   ],
   exports: [
